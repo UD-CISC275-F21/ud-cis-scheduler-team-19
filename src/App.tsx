0b5f1f4b18db55a48553dd5e4449163b6eb8ef9d
unknown <clongb@udel.edu>
2021-10-21 18:28:39 -0400

    added table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
+import { SemesterTable } from "./components/SemesterTable";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                UD CIS Scheduler
-                <p>
-                    Christopher Bao, Trent Littleton 
-                </p>
-            </header>
+            <h1>UD CIS Scheduler</h1>
+            <h4>Christopher Bao, Trent Littleton</h4>
+            <SemesterTable></SemesterTable>
         </div>
     );
 }
