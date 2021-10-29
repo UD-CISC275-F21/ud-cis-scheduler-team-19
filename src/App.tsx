aabe4a382e800c4f95d2442f04cfb9285e438f68
unknown <clongb@udel.edu>
2021-10-28 23:29:34 -0400

    made sidebar hideable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from "./logo.svg";
-import { SemesterTable } from "./components/SemesterTable";
 import { Sidebar } from "./components/Sidebar";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,9 +8,6 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <Sidebar></Sidebar>
-            <h1>UD CIS Scheduler</h1>
-            <h4>Christopher Bao, Trent Littleton</h4>
-            <SemesterTable></SemesterTable>
         </div>
     );
 }
