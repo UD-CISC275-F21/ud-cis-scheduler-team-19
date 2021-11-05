9c85fc05045c89e434958ada3b3f4b5d1c010005
unknown <clongb@udel.edu>
2021-11-04 21:41:52 -0400

    updated sidebar + css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Sidebar } from "./components/Sidebar";
+import { SemesterTable } from "./components/SemesterTable";
+import { Row } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
