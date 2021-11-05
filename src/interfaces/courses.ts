0220038c06ec6a438c7b35278382d8ca14a9c74d
Alex D <amdaley@udel.edu>
2021-11-04 23:03:37 -0400

    Created interface for course json file

---
+++ src/interfaces/courses.ts
@@ -0,0 +1,11 @@
+export interface Course{
+    code: string
+    name: string
+    descr: string
+    credits: number
+    preReq: string
+    limits: string
+    breadth: string
+    terms: string
+
+}
