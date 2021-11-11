7fd4b9083d92299d2b20461c6902ff1b392340d3
Alex D <amdaley@udel.edu>
2021-11-11 02:50:32 -0500

    Created course interface, and courseCard

---
+++ src/interfaces/courses.ts
@@ -0,0 +1,11 @@
+export interface Course{
+    prefix: string
+    name: string
+    description: string
+    credits: number
+    preReq: string
+    limits: string
+    breadth: string
+    terms: string
+
+}
