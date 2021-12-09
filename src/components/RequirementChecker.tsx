bae657a73057b5bfbea17415b8094e121dc0be94
unknown <trentlit@udel.edu>
2021-12-09 02:34:51 -0500

    Indents and key to iterator

--- src/components/RequirementChecker.tsx
+++ src/components/RequirementChecker.tsx
@@ -1,41 +1,41 @@
 import React from "react";
 
-const reqlist : string[] = [
-    "CISC108",
-    "CISC181",
-    "CISC210",
-    "CISC220",
-    "CISC260",
-    "CISC275",
-    "CISC303",
-    "CISC320",
-    "CISC361",
-    "CISC372",
-    "300 (Or Above) Technical Elective 1",
-    "300 (Or Above) Technical Elective 2",
-    "Focus Area Course 1",
-    "Focus Area Course 2",
-    "Focus Area Course 3",
-    "Focus Area Course 4",
-    "MATH205 or MATH350",
-    "MATH210",
-    "MATH241",
-    "MATH242",
-    "Capstone",
-    "Twelve Credits of Laboratory Science"
-  ];
+const reqlist: string[] = [
+  "CISC108",
+  "CISC181",
+  "CISC210",
+  "CISC220",
+  "CISC260",
+  "CISC275",
+  "CISC303",
+  "CISC320",
+  "CISC361",
+  "CISC372",
+  "300 (Or Above) Technical Elective 1",
+  "300 (Or Above) Technical Elective 2",
+  "Focus Area Course 1",
+  "Focus Area Course 2",
+  "Focus Area Course 3",
+  "Focus Area Course 4",
+  "MATH205 or MATH350",
+  "MATH210",
+  "MATH241",
+  "MATH242",
+  "Capstone",
+  "Twelve Credits of Laboratory Science"
+];
 
 const Reqcheck: React.FunctionComponent = () => {
-  return (        
-    <div style={styles.container}>       
-          {reqlist.map((value,index) => { 
-            return <div style={styles.reqItem} >
-            <span style={styles.courseID}>{reqlist[index]}</span>
-            <span>
-              <input type="checkbox"/>
-            </span>
-          </div>; 
-          })}
+  return (
+    <div style={styles.container}>
+      {reqlist.map((value, index) => {
+        return <div style={styles.reqItem} key = {index}>
+          <span style={styles.courseID}>{reqlist[index]}</span>
+          <span>
+            <input type="checkbox" />
+          </span>
+        </div>;
+      })}
     </div>
   );
 };
