563e29d461c72652f1f43e7efbdd714fab43b3df
unknown <trentlit@udel.edu>
2021-12-08 19:02:19 -0500

    Copy-pasted a line and forgot to change something

--- src/components/SemesterTable.tsx
+++ src/components/SemesterTable.tsx
@@ -27,25 +27,25 @@ export function SemesterTable({ schedule }:
                     {schedule[1] != null && <th>{schedule[1].prefix}</th>}
                     {schedule[1] != null && <th>{schedule[1].name}</th>}
                     {schedule[1] != null && <th>{schedule[1].credits}</th>}
-                    {schedule[0] != null && <th>{schedule[1].prereq}</th>}
+                    {schedule[1] != null && <th>{schedule[1].prereq}</th>}
                 </tr>
                 <tr>
                     {schedule[2] != null && <th>{schedule[2].prefix}</th>}
                     {schedule[2] != null && <th>{schedule[2].name}</th>}
                     {schedule[2] != null && <th>{schedule[2].credits}</th>}
-                    {schedule[0] != null && <th>{schedule[2].prereq}</th>}
+                    {schedule[2] != null && <th>{schedule[2].prereq}</th>}
                 </tr>
                 <tr>
                     {schedule[3] != null && <th>{schedule[3].prefix}</th>}
                     {schedule[3] != null && <th>{schedule[3].name}</th>}
                     {schedule[3] != null && <th>{schedule[3].credits}</th>}
-                    {schedule[0] != null && <th>{schedule[3].prereq}</th>}
+                    {schedule[3] != null && <th>{schedule[3].prereq}</th>}
                 </tr>
                 <tr>
                     {schedule[4] != null && <th>{schedule[4].prefix}</th>}
                     {schedule[4] != null && <th>{schedule[4].name}</th>}
                     {schedule[4] != null && <th>{schedule[4].credits}</th>}
-                    {schedule[0] != null && <th>{schedule[4].prereq}</th>}
+                    {schedule[4] != null && <th>{schedule[4].prereq}</th>}
                 </tr>
             </tbody>
         </Table>
